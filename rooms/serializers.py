b63db840fc3374550cf6e91f8962d63d648c3af1
urfrider <wnsl9704@naver.com>
2022-12-23 01:20:04 +0800

    created rest api for amenity and services

---
+++ rooms/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Amenity
+from .models import Room
+from users.serializers import BasicUserSerializer
+from categories.serializers import CategorySerializer
+
+
+class AmenitySerializer(ModelSerializer):
+    class Meta:
+        model = Amenity
+        fields = (
+            "name",
+            "description",
+        )
+
+
+class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+        )
+
+
+class RoomDetailSerializer(ModelSerializer):
+    owner = BasicUserSerializer(read_only=True)
+    amenities = AmenitySerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Room
+        fields = "__all__"
